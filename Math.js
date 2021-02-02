6b3dfa89fb9781d1255f3175015e133f3de582c3
trinlive <i.trin@live.com>
2021-02-02 17:54:14 +0700

    17.53-210202

--- Math.js
+++ Math.js
@@ -1,14 +1,53 @@
+var pi = Math.PI;    // 3.141592653589793
+console.log(`Math.PI :  ${pi}`);
+
 var x 
-
-x = Math.PI;
-console.log(`Math.PI : ${x}`);
-
-x = Math.round(4, 4);
+x = Math.round(4, 4);   // = 4 - rounded
 console.log(`Math.round(4, 4) : ${x}`);
 
-x = Math.round(4, 5);
-console.log(`Math.round(4, 5) ${x}`);
+x = Math.round(4, 5);  // = 5 - rounded
+console.log(`Math.round(4, 5) : ${x}`);
 
-console.log(`Math.round(4, 5) ${x}`);
-console.log(`Math.round(4, 5) ${x}`);
+x = Math.pow(2, 8); // 256 - 2 to the power of 8
+console.log(`Math.pow(2, 8) : ${x}`);
+
+x = Math.sqrt(49); // = 7 - square root
+console.log(`Math.sqrt(49) : ${x}`);
+
+x = Math.abs(-3.14); // = 3.14 - absolute, positive value
+console.log(`Math.abs(-3.14) : ${x}`);
+
+x = Math.ceil(3.14); // = 4 rounded up
+console.log(`Math.ceil(3.14) : ${x}`);
+
+x = Math.floor(3.99); // =3 rounded down 
+console.log(`Math.floor(3.99) : ${x}`);
+
+x = Math.sin(4); //  = 0.7568024953079282
+console.log(`Match.sin(4) : ${x}`);
+
+x = Math.cos(Math.PI);  // -1
+console.log(`Match.cos(Math.PI) : ${x}`);
+
+x = Math.min(0, 3, -2, 2); // -2
+console.log(`Match.min(0, 3, -2, 2) : ${x}`);
+
+x = Math.max(0, 3, -2, 2); // 3
+console.log(`Match.max(0, 3, -2, 2) : ${x}`);
+
+x = Math.log(1); // 0
+console.log(`Match.log(1) : ${x}`);
+
+x = Math.exp(1); // 2.718281828459045
+console.log(`Match.log(1) : ${x}`);
+
+x = Math.random(); // 
+console.log(`Match.random() : ${x}`);
+
+x = Math.floor(Math.random() * 100); //returns 0 to 99 
+console.log(`Match.floor(Math.random() * 100) : ${x}`);
+
+
+
+
 
