1674593452cc014263fe96c6a18c98a9ad088aad
trinlive <i.trin@live.com>
2021-02-02 17:42:31 +0700

    yes

---
+++ Math.js
@@ -0,0 +1,14 @@
+var x 
+
+x = Math.PI;
+console.log(`Math.PI : ${x}`);
+
+x = Math.round(4, 4);
+console.log(`Math.round(4, 4) : ${x}`);
+
+x = Math.round(4, 5);
+console.log(`Math.round(4, 5) ${x}`);
+
+console.log(`Math.round(4, 5) ${x}`);
+console.log(`Math.round(4, 5) ${x}`);
+
